77ba3091a0fbe9a3f25af94dbe4c3fb2bf336a93
Sweta Suman <swetaaaaa.suman12345@gmail.com>
2024-05-30 16:32:52 +0530

    calculator_new

---
+++ 3-calculator-version-two/src/components/Display.jsx
@@ -0,0 +1,13 @@
+import styles from "./Display.module.css";
+const Display = ({ displayValue }) => {
+  return (
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    ></input>
+  );
+};
+
+export default Display;
